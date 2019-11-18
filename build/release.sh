8f06cfa3cf3df66fa231a76eeeb5b99bc43d3fea
barret <47966933+Barretem@users.noreply.github.com>
2019-11-18 10:38:51 +0800

    fix: 调整发版脚本

--- build/release.sh
+++ build/release.sh
@@ -5,45 +5,16 @@ git pull origin dev
 git checkout master
 git merge dev
 
-VERSION=`npx select-version-cli`
 
-read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  echo "Releasing $VERSION ..."
+lerna publish --force-publish=* --exact --temp-tag --registry=http://129.204.96.188:4873
+conventional-changelog -p angular -i CHANGELOG.md -s -r 0
 
-  # build
-  VERSION=$VERSION npm run dist
+# commit
+git add -A
+git commit -m "[build] $VERSION"
 
-  # publish theme 不需要进行发版
-  # echo "Releasing theme-chalk $VERSION ..."
-  # cd packages/theme-chalk
-  # npm version $VERSION --message "[release] $VERSION"
-  # if [[ $VERSION =~ "beta" ]]
-  # then
-  #   npm publish --tag beta
-  # else
-  #   npm publish
-  # fi
-  # cd ../..
-
-  # commit
-  git add -A
-  git commit -m "[build] $VERSION"
-  npm version $VERSION --message "[release] $VERSION"
-
-  # publish
-  git push origin master
-  git push origin refs/tags/v$VERSION
-  git checkout dev
-  git rebase master
-  git push origin dev
-
-  if [[ $VERSION =~ "beta" ]]
-  then
-    lerna publish --force-publish=* --exact --dist-tag beta --registry=http://129.204.96.188:4873
-  else
-    lerna publish --force-publish=* --exact --registry=http://129.204.96.188:4873
-  fi
-fi
+# publish
+git push origin master
+git checkout dev
+git rebase master
+git push origin dev
