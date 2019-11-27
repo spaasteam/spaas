cf0ab1c75b42bb2369b1b32ea822e53e60106677
barret <47966933+Barretem@users.noreply.github.com>
2019-11-27 11:54:16 +0800

    feat: 更新子应用初始化模板

---
+++ packages/child-app/src/spaas-application/route.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const resolve = file => {
+  return path.resolve(__dirname, file);
+};
+
+exports.default = [
+  {
+    path: '/application-type',
+    component: resolve('./pages/application-type/index.vue'),
+    title: '应用管理',
+    appType: '1',
+    enable: false,
+  },
+  {
+    path: '/app-manage',
+    component: resolve('./pages/app-manage/index.vue'),
+    title: '应用类型',
+    appType: '0',
+    enable: true,
+  },
+];
